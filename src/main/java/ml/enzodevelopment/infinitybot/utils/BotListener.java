484fa603f492f5efb81f4447e9ebe89de3f60531
William Johnstone <37273113+GravityGamer@users.noreply.github.com>
2018-07-25 14:44:13 +0100

    added neat thing

--- src/main/java/ml/enzodevelopment/infinitybot/utils/BotListener.java
+++ src/main/java/ml/enzodevelopment/infinitybot/utils/BotListener.java
@@ -53,7 +53,62 @@ public class BotListener extends ListenerAdapter {
 
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
-
+        if (event.getChannel().getId().equals("471406980137746442")) {
+            if (event.getMessageId().equals("471661254746963988")) {
+                if (event.getReactionEmote().getName().equals("\u2714")) {
+                    if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471400678154043412"))) {
+                        event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471400678154043412")).queue();
+                    }
+                } else {
+                    event.getReaction().removeReaction().queue();
+                }
+            } else if (event.getMessageId().equals("471668210467471381")) {
+                if (event.getReactionEmote().isEmote()) {
+                    switch (event.getReactionEmote().getEmote().getName()) {
+                        case "copper":
+                            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471403052591611915"))) {
+                                event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471403052591611915")).queue();
+                            }
+                            event.getReaction().removeReaction().queue();
+                            break;
+                        case "bronze":
+                            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471403030647013376"))) {
+                                event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471403030647013376")).queue();
+                            }
+                            event.getReaction().removeReaction().queue();
+                            break;
+                        case "silver":
+                            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471403207760019476"))) {
+                                event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471403207760019476")).queue();
+                            }
+                            event.getReaction().removeReaction().queue();
+                            break;
+                        case "gold":
+                            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471403067552563233"))) {
+                                event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471403067552563233")).queue();
+                            }
+                            event.getReaction().removeReaction().queue();
+                            break;
+                        case "platinum":
+                            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471403080844312587"))) {
+                                event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471403080844312587")).queue();
+                            }
+                            event.getReaction().removeReaction().queue();
+                            break;
+                        case "diamond":
+                            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("471403115611029514"))) {
+                                event.getGuild().getController().addSingleRoleToMember(event.getMember(), event.getGuild().getRoleById("471403115611029514")).queue();
+                            }
+                            event.getReaction().removeReaction().queue();
+                        default:
+                            event.getReaction().removeReaction().queue();
+                            break;
+                    }
+                } else {
+                    event.getReaction().removeReaction().queue();
+                }
+            }
+        }
     }
 
     private boolean runChecks(GuildMessageReceivedEvent event, String botPrefix) {
